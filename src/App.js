938aa382654c42a529f605fd91cb1eb47f983120
Christian Nguyen <christiannguyen@169-231-173-78.wireless.ucsb.edu>
2017-05-03 12:22:21 -0700

    sorted data by day posted

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ class App extends Component {
       .then( response => {
         // response.results.length === 0 ? console.log('empty') : console.log('not empty');
         // console.log(response.data);
-        console.log(response.data.results);
+        console.log('results', response.data.results);
 
         const jobTitle = response.data.results.map(x => <div>{x.jobtitle}</div>);
         const snippet = response.data.results.map(x => x.snippet);
@@ -48,6 +48,9 @@ class App extends Component {
         const url = response.data.results.map(x => x.url);
         const datePosted = response.data.results.map(x => x.date);
         const dateSince = response.data.results.map(x => x.formattedRelativeTime);
+
+        response.data.results.sort((x,y) => x.formattedRelativeTime.substring(0,2) - y.formattedRelativeTime.substring(0,2));
+
         const jobs = response.data.results.map((x,index) => 
         <div className="diff-jobs-container container-fluid">
           <div className="row info">
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
 
+
+        const tipsList = ['Stay motivated.','Sell Yourself.','Don’t Limit Yourself to Online Applications.','Be Yourself.', 'Stay Positive.', 'Networking is an essential component of successful job hunting.', 'It’s acceptable to apply for the same job more than once.','Learn the STAR method.','Don’t stop applying for jobs.'];
+
+
 class Header extends Component{
     constructor(props){
         super(props);
         this.state = {
-            tips:[]
+            tips:['Don’t stop applying for jobs.']
         }
+    }
+    componentWillMount(){
+        let tipsPos = 0;
+        setInterval(() => {
+            if(tipsList.length - 1 > tipsPos){
+                this.setState({
+                    tips: tipsList[tipsPos]
+                })
+                tipsPos++;
+            }
+            else{
+                this.setState({
+                    tips: tipsList[tipsPos]
+                })
+                tipsPos = 0;
+            }
+        },3000);
+        
     }
     render(){
         return(
             <div className="header-container">
                 <h1> JobHunter</h1>
+                <p>{this.state.tips}</p>
             </div>
         );
     }

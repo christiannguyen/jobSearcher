85f80cc19271d64bff5a8960d77c8418323d0d88
Christian Nguyen <christiannguyen@Christians-MacBook-Pro.local>
2017-08-08 19:38:48 -0700

    updated code a bit

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import axios from 'axios';
 import Search from './components/Search';
 import Header from './components/Header';
 import Joblist from './components/Joblist';
-import Filter from './components/Filter';
-
+// import Filter from './components/Filter';
 
 
 class App extends Component {
@@ -21,47 +20,41 @@ class App extends Component {
       snippet: [],
       jobs: [],
       locationfilter: [],
-      height: window.innerHeight
+      height: window.innerHeight,
+      tipsList: ['Stay motivated.','Sell Yourself.','Don’t Limit Yourself to Online Applications.','Be Yourself.', 
+            'Stay Positive.', 'Networking is an essential component of successful job hunting.', 'It’s acceptable to apply for the same job more than once.','Learn the STAR method.',
+            'Don’t stop applying for jobs.'
+            ],
+      tipsPosition: Math.floor(Math.random() * 9)
     };
-    
-
-    this.fetchJobs.bind(this);
   }
 
   componentWillMount(){
-    this.fetchJobs(' ',' ','');
+    this.fetchJobs(' ',' ','',12);
     this.fetchCities();
   }
 
-  hey(){
-    console.log('hey');
+  componentDidMount(){
+    this.updateTips();
   }
 
   fetchCities(){
     axios.get('https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json')
       .then(response => {
-        
         const locationfilter = response.data.map(x => x);
         this.setState({locationfilter});
-        console.log('location state', this.state.locationfilter);
+        // console.log('location state', this.state.locationfilter);
       });
     }
 
-  fetchJobs(title, location,position){
+  fetchJobs = (title,location,position) => {
     
     const config = {
       headers: {'X-Mashape-Authorization': 'E1L7w8FFovmshox2kJWK2tZWjnD3p1prLkUjsn9QJHoI0lQyc8'}
       };
 //    axios.get('https://indeed-indeed.p.mashape.com/apisearch?publisher=1483245056846548&callback=<required>&chnl=<required>&co=<required>&filter=<required>&format=json&fromage=<required>&highlight=<required>&jt=<required>&l=austin%2C+tx&latlong=<required>&limit=25&q=java&radius=50&sort=<required>&st=<required>&start=<required>&useragent=<required>&userip=<required>&v=2',config)  
-    axios.get(`https://indeed-indeed.p.mashape.com/apisearch?publisher=1483245056846548&callback=<required>&chnl=<required>&co=<required>&filter=<required>&format=json&fromage=<required>&highlight=<required>&jt=${position}&l=${location}&latlong=<required>&limit=25&q=${title}&radius=50&sort=<required>&st=<required>&start=<required>&useragent=<required>&userip=<required>&v=2`,config)
+      axios.get(`https://indeed-indeed.p.mashape.com/apisearch?publisher=1483245056846548&callback=<required>&chnl=<required>&co=<required>&filter=<required>&format=json&fromage=<required>&highlight=<required>&jt=${position}&l=${location}&latlong=<required>&limit=1000&q=${title}&radius=50&sort=<required>&st=<required>&start=<required>&useragent=<required>&userip=<required>&v=2`,config)
       .then( response => {
-        // response.results.length === 0 ? console.log('empty') : console.log('not empty');
-        // console.log(response.data);
-        // if(response.data.results.length === 0){
-        //   console.log('error');
-        // }
-        console.log('results', response.data.results);
-
         const jobTitle = response.data.results.map(x => <div>{x.jobtitle}</div>);
         const snippet = response.data.results.map(x => x.snippet);
         const location = response.data.results.map(x => x.formattedLocationFull);
@@ -76,19 +69,16 @@ class App extends Component {
           <div className="row info">
             <h2 className="job-name" key={x.jobtitle} >{x.jobtitle}<span className="date-posted-since">{x.formattedRelativeTime}</span></h2>
             <h3 className="company-name" key={x.company}>{x.company}<span className="company-location"> {x.formattedLocationFull}</span></h3>
-            <p className="date-posted">Date Posted: {x.date.substring(0,17)}</p>
+            <p className="date-posted" key={x.date.substring(0,17)}>Date Posted: {x.date.substring(0,17)}</p>
           </div>
           <div className="row short-description">
             <p key={x.snippet.substring(0,3)}>{x.snippet}</p>
           </div>
           <div className="row url-link">
-            <a href={x.url} target="_blank" className="job-url">Posting</a>
+            <a key={x.url} href={x.url} target="_blank" className="job-url">Posting</a>
           </div>
         </div>);        
         
-
-        // console.log('jobtitle', jobTitle);
-        // console.log('blah';
         this.setState({ 
           jobTitle,
           snippet,
@@ -100,22 +90,28 @@ class App extends Component {
         });
         
         const windowHeight = document.getElementsByClassName('joblist-container')[0].offsetHeight;
-        console.log(windowHeight);
-        // console.log(this.state.jobs);
+        this.setState({
+          height: windowHeight
+        })
     });  
   }
 
+  updateTips = () => {
+    	let tipsPos = 0;
+			let len = this.state.tipsList.length;
+			setInterval(() => {
+        let tipsPosition = Math.floor(Math.random() * len);
+        this.setState({
+            tipsPosition
+        });
+			},4000);
+  }
+
   render() {
-    document.addEventListener('scroll', () => {
-      let x =  window.pageYOffset;
-      if(x > 5000)
-        this.hey();
-    });
-    
     return (
       <div className="app-container">
-        <Header />
-        <Search fetchJobs={this.fetchJobs.bind(this)}/>
+        <Header tips={this.state.tipsList[this.state.tipsPosition]}/>
+        <Search fetchJobs={this.fetchJobs} joblistHeight={this.state.height}/>
         <Joblist jobs={this.state.jobs} />
       </div>
     );
--- src/components/Search.js
+++ src/components/Search.js
@@ -2,77 +2,72 @@ import React, { Component } from 'react';
 
 
 class Search extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            title: '',
-            location: ''
-        }
-    }
-    handleTitleChange(event){
-        this.setState({title: event.target.value})
-    }
-    handleLocationChange(event){
-        this.setState({location: event.target.value})
+    componentWillMount(){
+        // this.canFetchMoreJobs();
     }
 
-    updateJobSearch(){
-        const title = this.state.title;
-        const location = this.state.location;
+		handleChange = (e) => {
+			e.preventDefault();
+		}
+
+    updateJobSearch = () => {
+				const title = document.querySelectorAll('input')[0].value;
+				const location = document.querySelectorAll('input')[1].value;				
         const s = document.getElementsByName('positions')[0];
         let position = s.options[s.selectedIndex].value;
-
+				
         if(position === 'all')
             position = '';
 
         this.props.fetchJobs(title,location,position);
     }
 
-    handleKeyPress(e){
-        const title = this.state.title;
-        const location = this.state.location;
+    handleKeyPress = (e) => {
+				const title = document.querySelectorAll('input')[0].value;
+				const location = document.querySelectorAll('input')[1].value;				
         const s = document.getElementsByName('positions')[0];
         let position = s.options[s.selectedIndex].value;
 
         if(position === 'all')
             position = '';
 
-        if(e.keyCode === 13)
-            this.props.fetchJobs(title,location,position);
+        if(e.keyCode === 13){
+            this.props.fetchJobs(title,location,position);            
+        }
     }
 
-    render(){
-        return(
-            <div onKeyDown={this.handleKeyPress.bind(this)} className="search-container">
-                <div className="search row">
-                     <select name="positions">
-                        <option value="all">All</option>                        
-                        <option value="fulltime">Full-Time</option>
-                        <option value="parttime">Part-Time</option>
-                        <option value="internship">Internship</option>
-                        <option value="contract">Contract</option>
-                        <option value="temporary">Temporary</option>
-                    </select>
-                    <span className="right-inner-addon">
-                        <i className="glyphicon glyphicon-briefcase"></i>
-                        <input type="text" onChange={this.handleTitleChange.bind(this)} placeholder="Job Title" value={this.state.title}/>
-                    </span>
-                   
-                    <span className="right-inner-addon">
-                        <i className="glyphicon glyphicon-globe"></i>
-                        <input type="text" onChange={this.handleLocationChange.bind(this)} placeholder="City, State, or Zipcode" value={this.state.location}/>
-                    </span>
-                    <span>
-                         <button onClick={this.updateJobSearch.bind(this)} className="search-button">Search</button>
-                    </span>
-                 </div>     
+    // canFetchMoreJobs(){
+    //     let windowHeight = this.props.joblistHeight;
+    //     console.log('window',windowHeight);
+    //     document.addEventListener('scroll', () => { 
+		// 			let x =  window.pageYOffset;
+    // }
 
-                { /*
-                <input type="text" onChange={this.handleTitleChange.bind(this)} placeholder="Job Title" value={this.state.title}/>
-                <input type="text" onChange={this.handleLocationChange.bind(this)} placeholder="Job Location" value={this.state.location}/>
-                <button onClick={this.updateJobSearch.bind(this)} className="search-button">Search</button>
-                */}
-            </div>
+    render(){
+			return(
+					<div onKeyDown={this.handleKeyPress} className="search-container">
+							<div className="search row">
+								<select name="positions">
+									<option value="all">All</option>                        
+									<option value="fulltime">Full-Time</option>
+									<option value="parttime">Part-Time</option>
+									<option value="internship">Internship</option>
+									<option value="contract">Contract</option>
+									<option value="temporary">Temporary</option>
+								</select>
+								<span className="right-inner-addon">
+									<i className="glyphicon glyphicon-briefcase"></i>
+									<input type="text" onChange={this.handleChange} placeholder="Job Title"/>
+								</span>
+								<span className="right-inner-addon">
+									<i className="glyphicon glyphicon-globe"></i>
+									<input type="text" onChange={this.handleChange} placeholder="City, State, or Zipcode"/>
+								</span>
+								<span>
+									<button onClick={this.updateJobSearch} className="search-button">Search</button>
+								</span>
+							</div>     
+					</div>
         );
     }
 }
